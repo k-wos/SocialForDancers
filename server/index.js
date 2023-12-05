c283b7bd2e362f1f300513a798b429533f3558d4
k-wos <kacper.wos33@gmail.com>
2023-12-05 20:35:43 +0100

    clear

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import dbConnection from "./dbConfig/index.js";
-import errorMiddleware from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 const app = express();
@@ -22,8 +21,6 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
-
-app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
