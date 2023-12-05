3b8476635ccb9fb9aa440c4106e4b0a0df632921
k-wos <kacper.wos33@gmail.com>
2023-12-05 12:27:40 +0100

    add compare string function

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,9 +1,14 @@
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 
-export const hashPassword = async (useValue) => {
+export const hashString = async (useValue) => {
     const salt = await bcrypt.genSalt(10);
 
     const hashedPassword = await bcrypt.hash(useValue, salt);
     return hashedPassword;
+};
+
+export const compareString = async (useValue, hashedValue) => {
+    const isMatch = await bcrypt.compare(useValue, hashedValue);
+    return isMatch;
 };
