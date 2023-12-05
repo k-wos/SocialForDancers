a5200792eee28dd9144634286479af711c029997
k-wos <kacper.wos33@gmail.com>
2023-12-05 23:34:24 +0100

    add regex for email

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+    password: { type: String, required: true, minlength: 6 },
     bio: String,
     picture: String,
     birthDate: Date,
