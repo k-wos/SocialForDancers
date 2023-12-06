02f48ab698a53c399c82b5b5a8c0af291d4f4942
k-wos <kacper.wos33@gmail.com>
2023-12-06 14:18:06 +0100

    add authCOntroller

---
+++ server/controllers/authController.js
@@ -0,0 +1,33 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+const router = express.Router();
+
+export const authenticate = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email) {
+        return res.status(400).json({ message: "Email is required" });
+    }
+    if (!password) {
+        return res.status(400).json({ message: "Password is required" });
+    }
+
+    const user = await User.findneOne({ email });
+
+    if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ token });
+};
+
+export default router;
