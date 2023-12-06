b699c6a8f41ee3d367090a08817198211ef6e18d
k-wos <kacper.wos33@gmail.com>
2023-12-06 16:53:49 +0100

    create post controller

---
+++ server/controllers/postController.js
@@ -0,0 +1,25 @@
+import { validationResult } from "express-validator";
+import Post from "../models/postModel";
+
+export const createPost = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ message: "Invalid Inputs" });
+    }
+
+    try {
+        const post = new Post({
+            title: req.body.title,
+            content: req.body.content,
+            creator: req.userId,
+            tags: req.body.tags,
+            media: req.body.media,
+        });
+        await post.save();
+
+        res.json({ post });
+    } catch (error) {
+        console.erroor(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
