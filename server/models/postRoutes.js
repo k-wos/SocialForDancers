3aa2c1b449153f5689f0dd3e971efff539be8de1
k-wos <kacper.wos33@gmail.com>
2023-12-06 16:57:59 +0100

    create post route

---
+++ server/models/postRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import auth from "../middleware/auth.js";
+import { createPost } from "../controllers/postController.js";
+import { check } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+    "/posts",
+    [
+        auth,
+        [
+            check("title", "Title is required").not().isEmpty(),
+            check("content", "Content is required").not().isEmpty(),
+        ],
+    ],
+    createPost
+);
+
+export default router;
