0a474dce138d5b3ab55887d907ee17281f94870d
k-wos <kacper.wos33@gmail.com>
2023-12-07 16:15:20 +0100

    set a tailwind colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    screens: {
-      sm: "640px",
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#f3c614",
+                secondary: "#353535",
+                light: "#f4f4f4",
+                dark: "#222",
+                danger: "#e3342f",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
+        screens: {
+            sm: "640px",
 
-      md: "768px",
+            md: "768px",
 
-      lg: "1024px",
+            lg: "1024px",
 
-      xl: "1280px",
+            xl: "1280px",
 
-      "2xl": "1536px",
+            "2xl": "1536px",
+        },
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
