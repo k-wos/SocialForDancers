c2a4343304b95146b974f94111eb7d9763870719
k-wos <kacper.wos33@gmail.com>
2023-12-07 17:31:37 +0100

    add tailwind classes and create Navbar component

--- client/src/components/homePage/Hero.jsx
+++ client/src/components/homePage/Hero.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import baner from "../../assets/baner.jpg";
 
 const Hero = () => {
@@ -10,9 +9,11 @@ const Hero = () => {
                 alt="dancing couple"
             ></img>
             <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
-                <div>
-                    <h1>Social for Dancers: Social Media dla tancerzy</h1>
-                    <p>
+                <div className="md:left-[10%] max-w-[1100px] m-auto absolute p-4">
+                    <h1 className="font-bold text-5xl md:text-6xl">
+                        Social for Dancers: Social Media dla tancerzy
+                    </h1>
+                    <p className="py-2">
                         Witaj w Social for Dancers - aplikacji, którą
                         stworzyliśmy specjalnie dla tancerzy. Poznaj naszą
                         społeczność, korzystając z unikalnych funkcji, które
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import Hero from "../components/homePage/Hero";
+import Navbar from "../components/homePage/Navbar";
 
 const HomePage = () => {
     return (
         <div>
+            <Navbar />
             <Hero />
         </div>
     );
