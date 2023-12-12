3ba8eddafab184c50d9f66a403cb196f17bf6bfb
k-wos <kacper.wos33@gmail.com>
2023-12-12 01:02:11 +0100

    add styles for items

--- client/src/components/homePage/YourProfile.jsx
+++ client/src/components/homePage/YourProfile.jsx
@@ -1,12 +1,20 @@
 import dancePhoto from "../../assets/dance.jpg";
 const YourProfile = () => {
     return (
-        <div>
-            <h2>Twój profil użytkownika</h2>
-            <div className="flex">
+        <div className="md:left-[10%] max-w-[1100px] m-auto p-8">
+            <h2 className="text-4xl py-6 bg-gradient-to-r from-purpleText to-orangeText nline-block text-transparent bg-clip-text">
+                Twój profil użytkownika
+            </h2>
+            <div className="flex gap-5">
                 <div>
-                    <img src={dancePhoto} alt="zdjęcie profilowe" />
-                    <h3>Wyeksponuj się</h3>
+                    <img
+                        src={dancePhoto}
+                        alt="zdjęcie profilowe"
+                        className="rounded-lg"
+                    />
+                    <h3 className="text-lg py-2 bg-gradient-to-r from-purpleText to-orangeText nline-block text-transparent bg-clip-text">
+                        Wyeksponuj się
+                    </h3>
                     <p>
                         Wstaw swoje najlepsze zdjęcia taneczne i filmy, aby
                         podzielić się swoimi wspaniałymi momentami na scenie lub
@@ -14,8 +22,14 @@ const YourProfile = () => {
                     </p>
                 </div>
                 <div>
-                    <img src={dancePhoto} alt="zdjęcie profilowe" />
-                    <h3>Dziel się umiejętnościami</h3>
+                    <img
+                        src={dancePhoto}
+                        alt="zdjęcie profilowe"
+                        className="rounded-lg"
+                    />
+                    <h3 className="text-lg py-2 bg-gradient-to-r from-purpleText to-orangeText nline-block text-transparent bg-clip-text">
+                        Dziel się umiejętnościami
+                    </h3>
                     <p>
                         Wstaw swoje najlepsze zdjęcia taneczne i filmy, aby
                         podzielić się swoimi wspaniałymi momentami na scenie lub
@@ -23,8 +37,14 @@ const YourProfile = () => {
                     </p>
                 </div>
                 <div>
-                    <img src={dancePhoto} alt="zdjęcie profilowe" />
-                    <h3>Nawiązuj kontakty</h3>
+                    <img
+                        src={dancePhoto}
+                        alt="zdjęcie profilowe"
+                        className="rounded-lg"
+                    />
+                    <h3 className="text-lg py-2 bg-gradient-to-r from-purpleText to-orangeText nline-block text-transparent bg-clip-text">
+                        Nawiązuj kontakty
+                    </h3>
                     <p>
                         Wstaw swoje najlepsze zdjęcia taneczne i filmy, aby
                         podzielić się swoimi wspaniałymi momentami na scenie lub
