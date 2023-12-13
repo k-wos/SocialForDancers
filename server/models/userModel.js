8554cb9d1f82971c191f519d7baf000aaacef219
k-wos <kacper.wos33@gmail.com>
2023-12-13 15:34:28 +0100

    add some changes to user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,22 +1,32 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
-    password: { type: String, required: true, minlength: 6 },
-    bio: String,
-    picture: String,
-    birthDate: Date,
-    danceClassLatin: String,
-    danceClassStandard: String,
-    prefferedDanceStyle: String,
-    favouriteDance: String,
-    height: Number,
-    city: String,
-    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "posts" }],
-});
+const userSchema = mongoose.Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/,
+        },
+        password: { type: String, required: true, minlength: 6 },
+        bio: String,
+        picture: String,
+        birthDate: Date,
+        danceClassLatin: String,
+        danceClassStandard: String,
+        prefferedDanceStyle: String,
+        favouriteDance: String,
+        height: Number,
+        city: String,
+        posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "posts" }],
+        follwers: { type: [String], default: [] },
+        followings: { type: [String], default: [] },
+    },
+    { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
     const user = this;
