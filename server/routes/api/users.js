aa1b0732e4a143b5d23a8794fe4b6b6e0bb645ae
k-wos <kacper.wos33@gmail.com>
2023-12-14 16:05:57 +0100

    Add validatio nto register request

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -8,9 +9,24 @@ router.get("/", (req, res) => {
 
 // @route   POST api/users
 // @desc    Register user
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("User route");
-});
+router.post(
+    "/",
+    [
+        check("firstName", "First name is required").not().isEmpty(),
+        check("lastName", "Last name is required").not().isEmpty(),
+        check("email", "Please include a valid email").isEmail(),
+        check(
+            "password",
+            "Please enter a password with 6 or more characters"
+        ).isLength({ min: 6 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send("User route");
+    }
+);
 
 export default router;
