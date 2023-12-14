3e04efc2c3decfcab707c9ac3801d19d5ea6ecd1
k-wos <kacper.wos33@gmail.com>
2023-12-14 23:54:54 +0100

    add auth user logic

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export default function (req, res, next) {
+export const auth = async function (req, res, next) {
     const token = req.header("x-auth-token");
 
     if (!token) {
@@ -15,4 +15,4 @@ export default function (req, res, next) {
     } catch (error) {
         res.status(401).json({ msg: "Token is not valid" });
     }
-}
+};
--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { auth } from "../../middleware/auth.js";
+import User from "../../models/User.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 export default router;
