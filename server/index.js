91e1fe1febe91e15597957d625875700e859f26f
k-wos <kacper.wos33@gmail.com>
2023-12-14 15:45:11 +0100

    import all routes to main js file

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import dbConnection from "./dbConfig/index.js";
+import userRoutes from "./routes/api/users.js";
+import postRoutes from "./routes/api/posts.js";
+import profileRoutes from "./routes/api/profile.js";
+import authRoutes from "./routes/api/auth.js";
 
 dotenv.config();
 
@@ -22,6 +26,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
---
+++ server/routes/api/profile.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Profile route");
+});
+
+export default router;
