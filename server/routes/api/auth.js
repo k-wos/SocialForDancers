b794680f6ea0c7cabdf576a437b4f225930cd07c
k-wos <kacper.wos33@gmail.com>
2023-12-15 00:04:47 +0100

    add login logic

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { auth } from "../../middleware/auth.js";
 import User from "../../models/User.js";
+import { check, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -13,5 +16,58 @@ router.get("/", auth, async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+router.post(
+    "/",
+    [
+        check("email", "Please include a valid email").isEmail(),
+        check("password", "Required").exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({
+                    errors: [{ msg: "Invalid Credentials" }],
+                });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({
+                    errors: [{ msg: "Invalid Credentials" }],
+                });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(
+                payload,
+                process.env.JWT_SECRET_KEY,
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server error");
+        }
+    }
+);
 
 export default router;
