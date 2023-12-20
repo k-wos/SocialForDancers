3f70f8bd8087c1cbf063ea2bf190159da1260af1
k-wos <kacper.wos33@gmail.com>
2023-12-20 01:45:07 +0100

    add unlike route

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -107,4 +107,30 @@ router.put("/like/:id", auth, async (req, res) => {
     }
 });
 
+router.put("/unlike/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (
+            (post.likes.filter(
+                (like) => like.user.toString() === req.user.id
+            ).length = 0)
+        ) {
+            return res.status(400).json({ msg: "Post not liked" });
+        }
+
+        const remove = post.likes
+            .map((like) => like.user.toString())
+            .indexOf(req.user.id);
+
+        post.likes.splice(remove, 1);
+
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
 export default router;
