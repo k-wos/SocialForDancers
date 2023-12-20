6dd3500b812708b43a13cfdb6ca4a901b99a3aa9
k-wos <kacper.wos33@gmail.com>
2023-12-20 01:18:31 +0100

    get all posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -36,4 +36,14 @@ router.post(
     }
 );
 
+router.get("/", auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
 export default router;
