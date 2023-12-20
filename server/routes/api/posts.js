3f28b91f86b23887aa24f5adb4c023f36dda44ab
k-wos <kacper.wos33@gmail.com>
2023-12-20 01:42:15 +0100

    add like route

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -86,4 +86,25 @@ router.delete("/:id", auth, async (req, res) => {
     }
 });
 
+router.put("/like/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+                .length > 0
+        ) {
+            return res.status(400).json({ msg: "Post already liked" });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
 export default router;
