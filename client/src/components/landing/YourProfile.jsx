fd1e42221cfdd07b159ce24ba8bb779b9e41f5fc
k-wos <kacper.wos33@gmail.com>
2023-12-22 01:11:11 +0100

    add card layout

--- client/src/components/landing/YourProfile.jsx
+++ client/src/components/landing/YourProfile.jsx
@@ -26,19 +26,21 @@ const YourProfile = () => {
             <h1 className="font-bolt text-5xl px-8 py-4 font-merriweather bg-gradient-to-r from-purpleText to-orangeText inline-block text-transparent bg-clip-text">
                 Twój Profil
             </h1>
-            <div className="grid md:grid-cols-3 gap-4 p-4">
+            <div className="grid md:grid-cols-3 gap-4 p-4 ">
                 {info.map((item, index) => (
-                    <div key={index} className="flex flex-col items-center p-4">
-                        <img
-                            src={item.img}
-                            className="rounded-xl shadow-md"
-                        ></img>
-                        <h1 className="font-bold text-2xl text-center pt-4">
-                            {item.title}
-                        </h1>
-                        <p className="text-center py-4 font-dmSans">
-                            {item.description}
-                        </p>
+                    <div
+                        key={index}
+                        className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                    >
+                        <img src={item.img} className="rounded-t-lg"></img>
+                        <div className="p-5">
+                            <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {item.title}
+                            </h1>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 font-dmSans">
+                                {item.description}
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>
