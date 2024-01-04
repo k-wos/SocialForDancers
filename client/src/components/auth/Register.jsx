c8261fe90c1e72d38678b6706fd91922de694b6e
k-wos <kacper.wos33@gmail.com>
2024-01-04 02:05:16 +0100

    add onSubit function

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -9,70 +9,85 @@ const Register = () => {
         password2: "",
     });
     const { firstName, lastName, email, password, password2 } = formdata;
+
+    const onChange = (e) =>
+        setFormdata({ ...formdata, [e.target.name]: e.target.value });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log("Passwords do not match");
+        } else {
+            console.log(formdata);
+        }
+    };
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl   lg:max-w-xl">
                 <h1 className="text-3xl font-semibold text-center text-indigo-700 underline uppercase">
                     Zarejestruj się
                 </h1>
-                <form className="mt-6">
+                <form className="mt-6" onSubmit={(e) => onSubmit(e)}>
                     <div className="mb-2">
-                        <label
-                            htmlFor="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
+                        <label className="block text-sm font-semibold text-gray-800">
                             Imię
                         </label>
                         <input
-                            type="email"
+                            type="text"
+                            name="firstName"
+                            value={firstName}
+                            onChange={(e) => onChange(e)}
+                            required
                             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
                     <div className="mb-2">
-                        <label
-                            htmlFor="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
+                        <label className="block text-sm font-semibold text-gray-800">
                             Nazwisko
                         </label>
                         <input
-                            type="email"
+                            type="text"
+                            name="lastName"
+                            value={lastName}
+                            onChange={(e) => onChange(e)}
+                            required
                             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
                     <div className="mb-2">
-                        <label
-                            htmlFor="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
+                        <label className="block text-sm font-semibold text-gray-800">
                             Email
                         </label>
                         <input
                             type="email"
+                            name="email"
+                            value={email}
+                            onChange={(e) => onChange(e)}
                             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
                     <div className="mb-2">
-                        <label
-                            htmlFor="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
+                        <label className="block text-sm font-semibold text-gray-800">
                             Hasło
                         </label>
                         <input
                             type="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => onChange(e)}
+                            required
                             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
                     <div className="mb-2">
-                        <label
-                            htmlFor="password2"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
+                        <label className="block text-sm font-semibold text-gray-800">
                             Potwierdź hasło
                         </label>
                         <input
-                            type="password2"
+                            type="password"
+                            name="password2"
+                            value={password2}
+                            onChange={(e) => onChange(e)}
                             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
