c21dee433d2475f52711840294316d84f79f1e11
k-wos <kacper.wos33@gmail.com>
2024-01-04 12:54:50 +0100

    add redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,20 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </Provider>
     );
 }
 
