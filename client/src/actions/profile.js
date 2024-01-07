cf026c89548afdec383f797ed2684a74d8f1153d
k-wos <kacper.wos33@gmail.com>
2024-01-07 02:07:04 +0100

    create profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getProfile, profileError } from "../reducers/profile";
+import { toast } from "react-toastify";
 
 export const getCurrentProfile = () => async (dispatch) => {
     try {
@@ -9,3 +10,28 @@ export const getCurrentProfile = () => async (dispatch) => {
         dispatch(profileError(err.response.data));
     }
 };
+
+export const createProfile =
+    (formData, history, edit = false) =>
+    async (dispatch) => {
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+
+            const res = await axios.post("/api/profile", formData, config);
+            dispatch(getProfile(res.data));
+            toast.success(edit ? "Profil zaktualizowany" : "Profil utworzony");
+
+            if (!edit) {
+                history.push("/dashboard");
+            }
+        } catch (err) {
+            dispatch(profileError(err.response.data));
+            toast.error(
+                err.response ? err.response.data.errors[0].msg : err.message
+            );
+        }
+    };
