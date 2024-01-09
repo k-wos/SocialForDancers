3f0bb29fafca464b9c0cd98858b0ae4a84b62254
k-wos <kacper.wos33@gmail.com>
2024-01-09 01:09:09 +0100

    create post actions

---
+++ client/src/actions/post.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+import { getPosts, postError } from "../reducers/post";
+
+export const getAllPosts = () => async (dispatch) => {
+    try {
+        const res = await axios.get("/api/posts");
+        dispatch(getPosts(res.data));
+    } catch (err) {
+        dispatch(postError(err.response.data));
+    }
+};
