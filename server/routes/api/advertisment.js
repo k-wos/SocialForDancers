444db31962bacebf7b0e82b5ae2aa8d3c21ffe5f
k-wos <kacper.wos33@gmail.com>
2024-01-10 15:12:55 +0100

    add get router for ad

--- server/routes/api/advertisment.js
+++ server/routes/api/advertisment.js
@@ -20,3 +20,25 @@ router.post("/", async (req, res) => {
         res.status(500).send(err.message);
     }
 });
+
+// @route   GET /ads
+// @desc    Get all ads
+router.get("/", async (req, res) => {
+    try {
+        const ads = await Ad.find();
+        res.json(ads);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// @route   GET /ads/:id
+// @desc    Get ad by id
+router.get("/:id", async (req, res) => {
+    try {
+        const ad = await Ad.findById(req.params.id);
+        res.json(ad);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
