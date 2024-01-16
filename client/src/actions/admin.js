437160dbe75ad30419530cfd7c490c70eb7da79d
k-wos <kacper.wos33@gmail.com>
2024-01-16 22:21:08 +0100

    add action for create user

--- client/src/actions/admin.js
+++ client/src/actions/admin.js
@@ -3,6 +3,9 @@ import {
     getUsersSuccess,
     getUsersFailure,
     getUsersUnauthorized,
+    createUserStart,
+    createUserSuccess,
+    createUserFailure,
 } from "../reducers/admin";
 
 export const getUsers = () => async (dispatch) => {
@@ -19,5 +22,21 @@ export const getUsers = () => async (dispatch) => {
         } else {
             dispatch(getUsersFailure(err.response.data));
         }
+    }
+};
+
+export const createUser = (userData) => async (dispatch) => {
+    dispatch(createUserStart());
+
+    try {
+        const res = await axios.post("/api/admin/addUser", userData, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+
+        dispatch(createUserSuccess(res.data));
+    } catch (err) {
+        dispatch(createUserFailure(err.response.data));
     }
 };
